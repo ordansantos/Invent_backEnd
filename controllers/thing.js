1a2360c3202327f55282ca1b9a168c6794f9fb83
Manuel Neto <manuel.neto@ccc.ufcg.edu.br>
2017-10-27 19:44:51 -0200

    Task: 05_Criação do Crud de thing

--- controllers/thing.js
+++ controllers/thing.js
@@ -27,6 +27,58 @@ router.get('/things', function (req, res) {
     
 });
 
+// :thing = id
+router.get('/thing/:thing', function(req, res, next) {
+    
+  var thing = req.thing;
+
+  Thing.find(function(err) {
+    if (err) {
+      res.sendStatus(404);
+      return next(err);
+    }
+      var r = res.send(thing);
+      console.log(r.data);
+
+      return next();
+  });
+  
+});
+
+router.delete('/thing/:thing', function(req, res, next) {
+    
+  Thing.remove({_id: req.params.thing}, function(err) {
+    if (err) {
+      res.sendStatus(404);
+
+       return next(err);
+    }
+    res.end();
+
+    return next();
+  });
+    
+});
+
+
+router.param('thing', function (req, res, next, _id) {
+  var query = Thing.findById(_id);
+
+  query.exec(function (err, thing) {
+    if (err) {
+      res.sendStatus(code404);
+      return next(err);
+    }
+    if (!thing) {
+      res.sendStatus(code404);
+      return next(new Error('can\'t find thing'));
+    }
+
+    req.thing = thing;
+    return next();
+  });
+});
+
 
 
 module.exports = router;
