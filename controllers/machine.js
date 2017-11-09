8a278ac7f413c595671e99b4a5ea124573d8897a
manuel.silva <manuel.silva@embedded.ufcg.edu.br>
2017-11-09 09:20:01 -0300

    atualizacao do CRUD de maquina

--- controllers/machine.js
+++ controllers/machine.js
@@ -26,6 +26,70 @@ router.get('/machines', function (req, res) {
 
 });
 
+// :machine = id
+router.get('/machine/:machine', function(req, res, next) {
+
+    var machine = req.machine;
+
+    Machine.find(function(err) {
+        if (err) {
+            res.sendStatus(404);
+            return next(err);
+        }
+        var r = res.send(machine);
+        console.log(r.data);
+
+        return next();
+    });
+
+});
+
+/**
+ * Edit Machine
+ */
+
+router.put('/machine/:id', function (req, res) {
+    Machine.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function (err, doc) {
+        if (err) console.log(err);
+        console.log("Objeto atualizado!");
+        res.send(doc);
+    });
+});
+
+router.delete('/machine/:machine', function(req, res, next) {
+
+    Machine.remove({_id: req.params.machine}, function(err) {
+        if (err) {
+            res.sendStatus(404);
+
+            return next(err);
+        }
+        res.end();
+
+        return next();
+    });
+
+});
+
+
+router.param('machine', function (req, res, next, _id) {
+    var query = Machine.findById(_id);
+
+    query.exec(function (err, machine) {
+        if (err) {
+            res.sendStatus(code404);
+            return next(err);
+        }
+        if (!machine) {
+            res.sendStatus(code404);
+            return next(new Error('can\'t find machine'));
+        }
+
+        req.machine = machine;
+        return next();
+    });
+});
+
 
 
 module.exports = router;
