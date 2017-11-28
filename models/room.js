9db8fcb594466afdf22acd4e1ae400acc6646ab3
manuel.silva <manuel.silva@embedded.ufcg.edu.br>
2017-11-28 17:25:33 -0300

    populando o banco de dados : Salas

--- models/room.js
+++ models/room.js
@@ -14,4 +14,34 @@ var rommSchema = mongoose.Schema({
 
 var Room = mongoose.model('Room', rommSchema);
 
+// ------------------- Populate Room collection on first run--------------------
+
+//var Room = mongoose.model('Room', rommSchema);
+
+var createRoom = function(nameRoom) {
+	new Room({
+		name: nameRoom,
+		things: []
+	}).save(function(error) {
+		if (error) {
+			console.error(" Data Insetion error! ");
+		}
+	});
+};
+
+Room.count({}, function(err, result) {
+	createRoom('Sala 100');
+	createRoom('Sala 101');
+	createRoom('Sala 102');
+	createRoom('Sala 103');
+	createRoom('Sala 104');
+	createRoom('Sala 105');
+	createRoom('Sala 106');
+	createRoom('Sala 200');
+	createRoom('Sala 201');
+	createRoom('Sala 202');
+	createRoom('Sala 203');
+});
+
+
 module.exports = Room;
