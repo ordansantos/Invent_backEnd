792aa6e0a7238bb344276e84b3e6fc8992ddea08
Ordan Santos <ordan.santos@gmail.com>
2017-12-13 22:27:14 -0300

    heroku

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
+
 var app = require('./config/express')();
 
-require('./config/database.js')('mongodb://localhost/invent-backEnd');
+require('./config/database.js')('mongodb://inventuser:invent123@ds125565.mlab.com:25565/heroku_6qnvmz38');
 
-
-// Authentication
-require('./config/passport');
 
 
 module.exports = app;
 
 var schedule = require('node-schedule');
-
