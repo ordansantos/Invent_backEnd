d78eb52ae799eb48432b9937b2b5603a140d7ead
Ordan Santos <ordan.santos@gmail.com>
2017-12-14 01:34:54 -0300

    heroku

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var app = require('./config/express')();
 
 require('./config/database.js')('mongodb://inventuser:invent123@ds125565.mlab.com:25565/heroku_6qnvmz38');
 
-
+// Authentication
+require('./config/passport');
 
 module.exports = app;
 
